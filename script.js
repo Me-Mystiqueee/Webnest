b20adfae18003c5bf7b95b2fd7ec137bcb04f5b9
Me-Mystiqueee <84579339+Me-Mystiqueee@users.noreply.github.com>
2021-12-15 17:54:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -48,8 +48,31 @@ submitButton.addEventListener('click', function() {
     if (userText.value === c) {
         output.classList.add("correctCaptcha");
         output.innerHTML = "Correct!";
-	a.title = "dec";
-	a.href="www.google.com";
+	var el_up = document.getElementById("GFG_UP");
+        var el_down = document.getElementById("GFG_DOWN");
+              
+            el_up.innerHTML = "Click on the button to generate "
+                    + "a link using JavaScript.";
+              
+            function GFG_Fun() {
+                  
+                // Create anchor element.
+                var a = document.createElement('a'); 
+                  
+                // Create the text node for anchor element.
+                var link = document.createTextNode("This is link");
+                  
+                // Append the text node to anchor element.
+                a.appendChild(link); 
+                  
+                // Set the title.
+                a.title = "This is Link"; 
+                  
+                // Set the href property.
+                a.href = "https://www.geeksforgeeks.org"; 
+                  
+                // Append the anchor element to the body.
+                document.body.appendChild(a); 
     } else {
         output.classList.add("incorrectCaptcha");
         output.innerHTML = "Incorrect, please try again";
